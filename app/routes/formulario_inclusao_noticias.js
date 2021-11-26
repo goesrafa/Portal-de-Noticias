f8a60e4598f684ca48f4d703d179bf9b5e852f5b
goesrafa <rafa.goes.2012@gmail.com>
2021-11-25 22:53:10 -0300

    inclusão da config do DB

--- app/routes/formulario_inclusao_noticias.js
+++ app/routes/formulario_inclusao_noticias.js
@@ -1,5 +1,5 @@
-module.exports = function(app){
-    app.get('/formulario_inclusao_noticias',function(req, res){
+module.exports = function (app) {
+    app.get('/formulario_inclusao_noticias', function (req, res) {
         res.render("admin/form_add_noticia")
     });
 }
