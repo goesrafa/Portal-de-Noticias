740efcf547bb59b3c4c801b3ffbbc59377b18db0
goesrafa <rafa.goes.2012@gmail.com>
2021-11-28 00:05:44 -0300

    Inserção das interações com os controllers

--- app/controllers/noticias.js
+++ app/controllers/noticias.js
@@ -10,8 +10,10 @@ module.exports.noticias = function(app, req, res){
 module.exports.noticia = function(app, req, res){
     var connection = app.config.dbConnection();
     var NoticiasDAO = new app.app.models.NoticiasDAO(connection);
+
+    var id_noticia = req.query;
     
-    NoticiasDAO.getNoticia(function(error, result){
+    NoticiasDAO.getNoticia(id_noticia,function(error, result){
         res.render("noticias/noticia",{noticia : result});
     });
 }
